6d8d183ec1bf05a53637775a274d58a29c1ea1c8
phantanlongzzz <phantanlong121102@gmail.com>
2026-07-02 04:23:31 +0700

    config f4 keys open terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,6 +19,33 @@ local function receive_problem()
     )
 end
 
+
+-- Nhấn F4 để mở Terminal dọc bên phải
+vim.keymap.set("n", "<F4>", function()
+  local current_dir = vim.fn.expand("%:p:h")
+
+  vim.cmd("vsplit")
+  vim.cmd("wincmd l") 
+  vim.cmd("vertical resize 45") 
+
+  vim.cmd("lcd " .. vim.fn.fnameescape(current_dir))
+
+  vim.cmd("terminal")
+
+  local win = vim.api.nvim_get_current_win()
+  vim.wo[win].number = false          
+  vim.wo[win].relativenumber = false  
+  vim.wo[win].signcolumn = "no"       
+  vim.wo[win].foldcolumn = "0"        
+  vim.wo[win].statuscolumn = ""       
+
+  vim.cmd("startinsert")
+end, { desc = "Mở Terminal dọc bên phải siêu đẹp" })
+
+
+
+
+
 keymap.set("n", "<F7>", receive_problem)
 
 keymap.set("n", "<F8>", "<cmd>CompetiTest run<CR>")
